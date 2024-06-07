2372170e8912f478a7a22adaedaa28dd47092fbd
MrugalN <mrugalnikhar@uconn.edu>
2024-06-07 16:00:56 -0400

    Enhance HTML structure and add images

--- scripts.js
+++ scripts.js
@@ -2,8 +2,35 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     // Add interactivity here
+
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('.navbar a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Alert on profile picture click
     const profilePic = document.querySelector(".profile-pic");
     profilePic.addEventListener("click", function() {
         alert("This is your profile picture!");
+    });
+
+    // Hover effect for project images
+    const projectImages = document.querySelectorAll('.project-img');
+    projectImages.forEach(image => {
+        image.addEventListener('mouseover', function() {
+            this.style.opacity = "0.7";
+            // You can add a description here if needed
+        });
+        image.addEventListener('mouseout', function() {
+            this.style.opacity = "1";
+        });
+        image.addEventListener('click', function() {
+            alert("This is a project image!");
+        });
     });
 });
